1320e5256a4ce0df6ba945ed92242812f9667bc0
Isaque Araújo <isaquearaujo.profissional@gmail.com>
2022-04-22 22:11:36 -0300

    Adicionando ao aplicativo

--- stacks/index.js
+++ stacks/index.js
@@ -1,5 +1,11 @@
+import FrontendStack from "./FrontendStack";
+
 import StorageStack from "./StorageStack";
 
 export default function main(app) {
-  new StorageStack(app, "storage");
+  const storageStack = new StorageStack(app, "storage");
+
+  new FrontendStack(app, "frontend", {
+    bucket: storageStack.bucket,
+  });
 }
