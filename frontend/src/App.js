8c0196edd78f5a446db4bf9d1108bb56cedbea75
Isaque Araújo <isaquearaujo.profissional@gmail.com>
2022-04-22 22:26:48 -0300

    Criando formulário e executando a função lambda via gatewayAPI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react';
+import './App.css';;
+ 
 function App() {
+  const [campos, setCampos] = useState({
+    nome: '',
+    email: '',
+    mensagem: ''
+  });
+
+  function handleInputChange(event){
+    campos[event.target.name] = event.target.value;
+    setCampos(campos);
+  }
+
+  function handleFormSubmit(event){
+      event.preventDefault();
+      console.log(campos);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(
+      "https://ixnvihz6u4.execute-api.sa-east-1.amazonaws.com/sendEmail",
+      {
+        mode: "no-cors",
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: campos.nome,
+          mensagem : campos.mensagem,
+          email : campos.email,
+        }),
+      }
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="child">
+    <div className="container">
+      <form id="contact-form" onSubmit={handleFormSubmit}>
+        <div className="title">
+          <label>Envie uma mensagem anônima para seu crush!</label>
+        </div>
+        <label htmlFor="nome">Nome</label>
+        <input type="text" id="nome" name="nome" placeholder="Nome da pessoa.." onChange={handleInputChange}/>
+ 
+        <label htmlFor="mensagem">Mensagem</label>
+        <textarea id="mensagem" name="mensagem" placeholder="Escreva algo.." className="textArea" onChange={handleInputChange}></textarea>
+
+        <label htmlFor="email">E-mail</label>
+        <input type="text" id="email" name="email" placeholder="E-mail de destino.." onChange={handleInputChange}/>
+ 
+        <input type="submit" onClick={handleSubmit} value="Enviar" />
+      </form>
     </div>
+    </div>
+    
   );
 }
-
+ 
 export default App;
