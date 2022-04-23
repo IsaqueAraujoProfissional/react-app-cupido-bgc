8fe00111660265260a25fd4ad3abf6047c7ff7e0
Isaque Araújo <isaquearaujo.profissional@gmail.com>
2022-04-22 22:16:05 -0300

    Consertando referência ao bucket

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -1,7 +1,10 @@
 import * as sst from "@serverless-stack/resources";
 
 export default class StorageStack extends sst.Stack {
-  constructor(scope, id, props) {
+    bucket;
+  
+    constructor(scope, id, props) {
     super(scope, id, props);
+    this.bucket = new sst.Bucket(this, "Uploads");
   }
 }
